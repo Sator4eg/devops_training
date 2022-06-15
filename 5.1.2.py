30d3df06bc9b8b5d2eb9d9ab2acf4ca1807874a4
Sator4eg <sator4eg@gmail.com>
2022-06-15 17:23:15 +0300

    initial commit

---
+++ 5.1.2.py
@@ -0,0 +1,22 @@
+a1 = int(input('enter a1 '))
+b1 = int(input('enter b1 '))
+a2 = int(input('enter a2 '))
+b2 = int(input('enter b2 '))
+cal1 = str()
+cal2 = str()
+if a1 % 2 != 0 and b1 % 2 != 0:
+    cal1 = 'white'
+elif a1 % 2 == 0 and b1 % 2 == 0:
+    cal1 = 'white'
+else:
+    cal1 = 'black'
+if a2 % 2 != 0 and b2 % 2 != 0:
+    cal2 = 'white'
+elif a2 % 2 == 0 and b2 % 2 == 0:
+    cal2 = 'white'
+else:
+    cal2 = 'black'
+if cal1 == cal2:
+    print(cal1, 'Yes')
+else:
+    print(cal1, cal2, 'no')
